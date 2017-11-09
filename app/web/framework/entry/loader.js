530129f95f7d18b8ff34ec91f27daeef94764af4
hubcarl <hubcarl@126.com>
2017-11-09 11:14:15 +0800

    feat:react 16

--- app/web/framework/entry/loader.js
+++ app/web/framework/entry/loader.js
@@ -1,10 +1,11 @@
 module.exports = function(source) {
+  this.cacheable();
   const resourcePath = this.resourcePath;
   return `
     import React from 'react';
     import ReactDom from 'react-dom';
     import App from '${this.resourcePath.replace(/\\/g, '\\\\')}';
     const state = window.__INITIAL_STATE__;
-    ReactDom.render(<App {...state} />, document.getElementById('app'));
+    ReactDom.hydrate(<App {...state} />, document.getElementById('app'));
   `;
 };
--- config/plugin.js
+++ config/plugin.js
@@ -1,10 +1,5 @@
 exports.static = true;
 
-exports.react = {
-  enable: true,
-  package: 'egg-view-react'
-};
-
 exports.reactssr = {
   enable: true,
   package: 'egg-view-react-ssr'
