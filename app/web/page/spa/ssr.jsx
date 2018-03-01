e210b75f8c4f464e4b62c23bde4f8d6de8132713
hubcarl <hubcarl@126.com>
2018-03-01 23:15:17 +0800

    fix: spa hot update

--- app/web/page/spa/ssr.jsx
+++ app/web/page/spa/ssr.jsx
@@ -3,28 +3,33 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import {match, RouterContext} from 'react-router'
 import { BrowserRouter, StaticRouter } from 'react-router-dom';
-import { matchRoutes, renderRoutes } from 'react-router-config'
+import { matchRoutes, renderRoutes } from 'react-router-config';
+import { AppContainer } from 'react-hot-loader';
 import Layout from 'framework/layout/layout.jsx';
 import Header from 'component/header/header';
 import SSR from 'component/spa/ssr/ssr';
 import { create } from 'component/spa/ssr/store';
 import routes from 'component/spa/ssr/routes'
-import './spa.css';
 
 const clientRender = () => {
   const store = create(window.__INITIAL_STATE__);
   const url = store.getState().url;
-  ReactDOM.render(
-    <div>
-      <Header></Header>
-      <Provider store={ store }>
-        <BrowserRouter>
-          <SSR url={ url }/>
-        </BrowserRouter>
-      </Provider>
-    </div>,
-    document.getElementById('app')
+  const Entry = () => (<div>
+    <Header></Header>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <SSR url={ url }/>
+      </BrowserRouter>
+    </Provider>
+  </div>
   );
+  const render = (App)=>{
+    ReactDOM.hydrate(EASY_ENV_IS_DEV ? <AppContainer><App /></AppContainer> : <App />, document.getElementById('app'));
+  };
+  if (EASY_ENV_IS_DEV && module.hot) {
+    module.hot.accept();
+  }
+  render(Entry);
 };
 
 const serverRender = (context, options)=> {
@@ -55,8 +60,4 @@ const serverRender = (context, options)=> {
     )
   });
 };
-
 export default EASY_ENV_IS_NODE ?  serverRender : clientRender();
-
-
-
