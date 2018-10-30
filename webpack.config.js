6ba0853849a52bcf399fdc4ef509a120d699ce09
sky <hubcarl@126.com>
2018-10-30 15:35:38 +0800

    feat: upgrade easywebpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,16 @@
 module.exports = {
-  egg: true,
-  framework: 'react',
-  // devtool: 'source-map',
+  devtool: 'eval',
   entry: {
-    include: ['app/web/page',
-      { layout: 'app/web/framework/layout/layout.jsx?loader=false' }
-    ],
+    include: ['app/web/page'],
     exclude: ['app/web/page/[a-z]+/component', 'app/web/page/test'],
     loader: {
       client: 'app/web/framework/entry/client-loader.js',
       server: 'app/web/framework/entry/server-loader.js'
     }
   },
-  alias: {
-    asset: 'app/web/asset',
-    component: 'app/web/component',
-    framework: 'app/web/framework',
-    store: 'app/web/store'
-  },
   dll: ['react', 'react-dom'],
-  loaders: {
-
-  },
-  plugins: {
-
-  },
+  loaders: {},
+  plugins: {},
   done() {
     console.log('---webpack compile finish---');
   }
