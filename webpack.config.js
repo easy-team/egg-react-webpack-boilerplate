1a1ba3167b13a129e2adb6e5457464dda6d940bc
sky <hubcarl@126.com>
2018-11-03 22:48:29 +0800

    feat: adapter babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: {
-    app: 'app/web/page/app/index.js',
-    list: 'app/web/page/list/index.jsx'
+    app: 'app/web/page/app/index.js'
+    // list: 'app/web/page/list/index.jsx'
   }
 };
