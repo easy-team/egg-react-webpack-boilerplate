ee20ddba50fdf188482ca868a3f4708e29ea34f9
sky <hubcarl@126.com>
2019-06-26 17:17:28 +0800

    feat: add async message description

--- app/web/page/app/components/widget/async-image.jsx
+++ app/web/page/app/components/widget/async-image.jsx
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 import Logo from '../../../../asset/images/react.png';
 export default class AsyncImage extends React.Component {
     render() {
-        return <img src={Logo} />
+        return <div><img src={Logo} /><div>这是异步加载的内容</div></div>
     }
 }
