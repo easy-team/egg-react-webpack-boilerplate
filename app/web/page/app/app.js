1ae878d1c2fbc20db96b86012dab965eb7d182ae
sky <hubcarl@126.com>
2019-07-30 15:50:01 +0800

    fix: page flicker

--- app/web/page/app/app.js
+++ app/web/page/app/app.js
@@ -5,15 +5,23 @@ import Header from '../../component/header/header.jsx';
 import App from './route';
 import store from './store';
 
+const render = () => {
+  ReactDOM.render(
+    <div>
+      <Header></Header>
+      <Provider store={store}>
+        <App url = { store.getState().url } />
+      </Provider>
+    </div>,
+    document.getElementById('app')
+  );
+}
+
 if (EASY_ENV_IS_DEV) {
   module.hot.accept();
+  // fix dev mode, page flicker
+  setTimeout(render, 100);
+} else {
+  render();
 }
-ReactDOM.render(
-  <div>
-    <Header></Header>
-    <Provider store={store}>
-      <App url = { store.getState().url } />
-    </Provider>
-  </div>,
-  document.getElementById('app')
-);
+
