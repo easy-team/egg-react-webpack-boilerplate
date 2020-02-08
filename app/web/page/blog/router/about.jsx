5176bd098f9ac6f2d272b474723e87813c77341b
sky <hubcarl@126.com>
2020-02-08 14:18:57 +0800

    fix: doc link

--- app/web/page/blog/router/about.jsx
+++ app/web/page/blog/router/about.jsx
@@ -12,7 +12,7 @@ export default class About extends Component {
     };
     return <div>
       <h2 style={{...styleTitle}}>Egg React Server Side Render</h2>
-      <h4 style={{...styleSub}}><a href="https://github.com/easy-team/egg-react">https://github.com/easy-team/egg-react</a></h4>
+      <h4 style={{...styleSub}}><a href="https://github.com/easy-team/egg-react-webpack-boilerplate">https://github.com/easy-team/egg-react-webpack-boilerplate</a></h4>
       <h4 style={{...styleSub}}><a href="https://yuque.com/easy-team/egg-react">https://yuque.com/easy-team/egg-react</a></h4>
       <h4 style={{...styleSub}}><a href="https://easyjs.cn/egg-react">https://easyjs.cn/egg-react</a></h4>
     </div>;
